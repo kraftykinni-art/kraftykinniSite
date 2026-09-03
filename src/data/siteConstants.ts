6e707c80ae2a9361c6738a444067b83a1214444f
Navneet Arya <navneet.arya@bold.com>
2026-09-03 12:37:03 +0530

    Update

--- src/data/siteConstants.ts
+++ src/data/siteConstants.ts
@@ -6,4 +6,8 @@ export const KRAFTYKINNI_SAME_AS = [
   'https://www.linkedin.com/in/kraftykinni/',
   'https://www.youtube.com/@kraftykinni',
   'https://in.pinterest.com/KraftyKinni/',
+  'https://share.google/QCgOn0bFdiRRe5OFX',
+  'https://www.google.com/maps/place/KraftyKinni/@28.5032749,77.3817466,17z/data=!3m1!4b1!4m6!3m5!1s0x390ce9fb49d4e935:0xbed5ad5b5362b002!8m2!3d28.5032702!4d77.3843215!16s%2Fg%2F11svwnn70v',
+  // TODO: add the Wikidata item URL here once created, e.g.:
+  // 'https://www.wikidata.org/wiki/Q123456789',
 ];
